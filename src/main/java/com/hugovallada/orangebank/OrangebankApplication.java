e7f69ad269a1350721fe7eae1e3a440429d0df06
Hugo Vallada <valladahugo@gmail.com>
2020-12-28 15:54:19 -0300

    First commit

---
+++ src/main/java/com/hugovallada/orangebank/OrangebankApplication.java
@@ -0,0 +1,13 @@
+package com.hugovallada.orangebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangebankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hugovallada/orangebank/OrangebankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugovallada.orangebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
