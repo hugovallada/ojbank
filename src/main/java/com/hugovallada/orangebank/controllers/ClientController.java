664379ccadc98767b330b1f5d779e2a1f38ec656
Hugo Vallada <valladahugo@gmail.com>
2020-12-28 16:58:31 -0300

    Add entity client, and controller basic simulation

--- src/main/java/com/hugovallada/orangebank/controllers/ClientController.java
+++ src/main/java/com/hugovallada/orangebank/controllers/ClientController.java
@@ -1,7 +1,13 @@
 package com.hugovallada.orangebank.controllers;
 
+import java.time.Instant;
+import java.time.LocalDate;
+
+import com.hugovallada.orangebank.domain.clients.Client;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/clients")
 public class ClientController {
 
-    @GetMapping
-    public ResponseEntity<String> getClients(){
-        return ResponseEntity.status(200).body("All Work");
+    @GetMapping("/{name}")
+    public ResponseEntity<?> getClientByName(@PathVariable String name){
+        //TODO: Create Repositorys
+        if(name.equals("Hugo")){
+            return ResponseEntity.status(404).body("The client already exists");
+        }
+
+        var client = new Client();
+        client.setName(name);
+        client.setEmail("example@gmail.com");
+        client.setCpf("0000000000");
+        client.setBirthDate(LocalDate.parse("1999-02-09"));
+        
+        return ResponseEntity.status(200).body(client);
     }
     
 }
---
+++ src/main/java/com/hugovallada/orangebank/domain/clients/Client.java
@@ -0,0 +1,36 @@
+package com.hugovallada.orangebank.domain.clients;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+}
