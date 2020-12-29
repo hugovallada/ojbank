37fe7cf76c46181671064109ab40478333c67ca6
Hugo Vallada <valladahugo@gmail.com>
2020-12-29 11:39:53 -0300

    Add Client repository and service

--- src/main/java/com/hugovallada/orangebank/controllers/ClientController.java
+++ src/main/java/com/hugovallada/orangebank/controllers/ClientController.java
@@ -1,10 +1,11 @@
 package com.hugovallada.orangebank.controllers;
 
-import java.time.Instant;
 import java.time.LocalDate;
 
+import com.hugovallada.orangebank.application.services.ClientService;
 import com.hugovallada.orangebank.domain.clients.Client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/clients")
 public class ClientController {
+
+    private ClientService clientService;
+
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
 
     @GetMapping("/{name}")
     public ResponseEntity<?> getClientByName(@PathVariable String name){
---
+++ src/main/java/com/hugovallada/orangebank/domain/clients/ClientRepository.java
@@ -0,0 +1,6 @@
+package com.hugovallada.orangebank.domain.clients;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
