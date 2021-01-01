e9f89d11530884635ea38eef3e66eda254df21f5
Hugo Vallada <valladahugo@gmail.com>
2021-01-01 12:13:50 -0300

    Add automapper, dto and validation

--- src/main/java/com/hugovallada/orangebank/domain/clients/Client.java
+++ src/main/java/com/hugovallada/orangebank/domain/clients/Client.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
